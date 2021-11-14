4b9656ce3e996b61121cb11447b812bb062904d9
Bariza Arfadillah Abqariy <94269366+2010bariza@users.noreply.github.com>
2021-11-14 10:17:48 +0700

    Add files via upload

---
+++ PublicRepository.py
@@ -0,0 +1,12 @@
+Nama = 'Bariza Arfadillah Abqariy'
+NIM = 212410102010
+Program_Studi = 'Teknologi Informasi'
+Fakultas = 'Ilmu Komputer'
+Asal = 'Situbondo'
+
+print(
+    'Halo!! Perkenalkan namaku', Nama,', NIM', NIM,',' 
+    'dari program studi', Program_Studi,', Fakultas', Fakultas,
+    'Universitas Jember.' 'Aku berasal dari', Asal,'.'
+    'Salam Kenal Semua!!!'
+)
